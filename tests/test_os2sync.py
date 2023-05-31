4c8b79a9bbcbe0da33fb8e1f52a524d68a349ded
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2023-05-31 09:21:19 +0200

    feat: always write orgunits to os2sync.

--- tests/test_os2sync.py
+++ tests/test_os2sync.py
@@ -19,13 +19,13 @@ o2 = OrgUnit(
 
 @patch("os2sync_export.config.get_os2sync_settings", return_value=dummy_settings)
 def test_os2sync_upsert_org_unit_no_changes(get_settings_mock):
-    """Test that if there are no changes to an org_unit we won't write to os2sync"""
+    """Test that if there are no changes to an org_unit we still write to os2sync"""
     from os2sync_export.os2sync import upsert_org_unit
 
     with patch("os2sync_export.os2sync.os2sync_get_org_unit", return_value=o):
         with patch("os2sync_export.os2sync.os2sync_post") as post_mock:
             upsert_org_unit(o, "os2sync_api_url")
-            post_mock.assert_not_called()
+            post_mock.assert_called_once_with("{BASE}/orgUnit/", json=o.json())
 
 
 @patch("os2sync_export.config.get_os2sync_settings", return_value=dummy_settings)
@@ -33,7 +33,7 @@ def test_os2sync_upsert_org_unit_new(get_settings_mock):
     """Test that if no orgUnit was found in fk-org we create it."""
     from os2sync_export.os2sync import upsert_org_unit
 
-    with patch("os2sync_export.os2sync.os2sync_get_org_unit", side_effect=KeyError()):
+    with patch("os2sync_export.os2sync.os2sync_get_org_unit", side_effect=KeyError):
         with patch("os2sync_export.os2sync.os2sync_post") as post_mock:
             upsert_org_unit(o, "os2sync_api_url")
             post_mock.assert_called_once_with("{BASE}/orgUnit/", json=o.json())
@@ -62,7 +62,7 @@ def test_os2sync_upsert_org_unit_keep_fk_fields(get_settings_mock):
 
             upsert_org_unit(o, "os2sync_api_url")
 
-            post_mock.assert_not_called()
+            post_mock.assert_called_once_with("{BASE}/orgUnit/", json=o2.json())
 
 
 @patch("os2sync_export.config.get_os2sync_settings", return_value=dummy_settings)
@@ -100,4 +100,4 @@ def test_os2sync_upsert_org_unit_ordered_tasks(get_settings_mock):
 
             upsert_org_unit(org_unit, "os2sync_api_url")
 
-            post_mock.assert_not_called()
+            post_mock.assert_called_once_with("{BASE}/orgUnit/", json=org_unit.json())
