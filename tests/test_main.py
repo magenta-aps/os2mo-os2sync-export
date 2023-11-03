90036733b458fbf1d6966240ae8bfaea01c49b70
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2023-11-03 12:46:15 +0100

    test: rewrite test with pytest parametrize

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,16 +12,13 @@ from os2sync_export.os2sync_models import OrgUnit
 
 
 @pytest.mark.asyncio
+@pytest.mark.parametrize("overwritten_uuid", (True, False))
 @patch(
     "os2sync_export.main.get_ituser_org_unit_and_employee_uuids",
     return_value=(None, uuid4()),
 )
-@patch(
-    "os2sync_export.main.is_relevant",
-    return_value=True,
-)
 async def test_trigger_it_user_update(
-    is_relevant_mock, get_mock, mock_context, mock_settings
+    get_mock, overwritten_uuid, mock_context, mock_settings
 ):
     """Test react to it-event and update user
 
@@ -29,69 +26,37 @@ async def test_trigger_it_user_update(
     with one fk-org account found with uuid matching MOs
     """
     mock_context["user_context"]["settings"] = mock_settings
-    settings, _, os2sync_client = unpack_context(context=mock_context)
+    settings, gql_session, os2sync_client = unpack_context(context=mock_context)
+
     # MO persons uuid
-    user_uuid = get_mock.return_value[1]
+    _, user_uuid = get_mock.return_value
     # Fk-org users for this person - same uuid as i os2mo
-    fk_org_users = [{"Uuid": user_uuid}]
+    fk_org_users = [{"Uuid": uuid4() if overwritten_uuid else user_uuid}]
     with patch(
         "os2sync_export.main.get_sts_user", return_value=fk_org_users
     ) as get_user_mock:
         await amqp_trigger_it_user(mock_context, uuid4(), None)
     get_mock.assert_awaited_once()
-    get_user_mock.assert_called_once_with(user_uuid, gql_session=_, settings=settings)
+    get_user_mock.assert_called_once_with(
+        user_uuid, gql_session=gql_session, settings=settings
+    )
     # Test that we call update_user on events
     os2sync_client.update_users.assert_called_once_with(user_uuid, fk_org_users)
-    # Ensure we won't delete the user
-    os2sync_client.delete_user.assert_not_called()
+    if overwritten_uuid:
+        os2sync_client.delete_user.assert_called_with(user_uuid)
+    else:
+        os2sync_client.delete_user.assert_not_called()
 
 
 @pytest.mark.asyncio
-@patch(
-    "os2sync_export.main.get_ituser_org_unit_and_employee_uuids",
-    return_value=(None, uuid4()),
-)
-@patch(
-    "os2sync_export.main.is_relevant",
-    return_value=True,
-)
-async def test_trigger_it_user_delete_mo_uuid(
-    is_relevant_mock, get_mock, mock_context, mock_settings
-):
-    """Delete fk-org user with MO uuid if a uuid is overwritten from an IT-account
-
-    Tests that if the found it-account(s) has different uuids to MOs
-    the account with MOs uuid is deleted
-    """
-    mock_context["user_context"]["settings"] = mock_settings
-    settings, _, os2sync_client = unpack_context(context=mock_context)
-    # MO persons uuid
-    user_uuid = get_mock.return_value[1]
-    # Fk-org users for this person with uuids different from os2mo
-    fk_org_users = [{"Uuid": uuid4()}, {"Uuid": uuid4()}]
-    with patch(
-        "os2sync_export.main.get_sts_user", return_value=fk_org_users
-    ) as get_user_mock:
-        await amqp_trigger_it_user(mock_context, uuid4(), None)
-    get_mock.assert_awaited_once()
-    get_user_mock.assert_called_once_with(user_uuid, gql_session=_, settings=settings)
-    # Test that we call update_user on events
-    os2sync_client.update_users.assert_called_once_with(user_uuid, fk_org_users)
-    # Ensure that we delete the fk-org user with MO uuid as the fk-org accounts have different uuids
-    os2sync_client.delete_user.assert_called_with(user_uuid)
-
-
-@pytest.mark.asyncio
+@pytest.mark.parametrize("is_relevant", (True, False))
+@pytest.mark.parametrize("overwritten_uuid", (True, False))
 @patch(
     "os2sync_export.main.get_ituser_org_unit_and_employee_uuids",
     return_value=(uuid4(), None),
 )
-@patch(
-    "os2sync_export.main.is_relevant",
-    return_value=True,
-)
 async def test_trigger_it_orgunit_update(
-    is_relevant_mock, get_mock, mock_context, mock_settings
+    get_mock, overwritten_uuid, is_relevant, mock_context, mock_settings
 ):
     """Test react to it-event and update orgunit
 
@@ -102,45 +67,26 @@ async def test_trigger_it_orgunit_update(
     settings, _, os2sync_client = unpack_context(context=mock_context)
 
     orgunit_uuid = get_mock.return_value[0]
-    fk_org_orggunit = OrgUnit(Uuid=orgunit_uuid)
+    fk_org_orggunit = (
+        OrgUnit(Uuid=uuid4()) if overwritten_uuid else OrgUnit(Uuid=orgunit_uuid)
+    )
     with patch(
         "os2sync_export.main.get_sts_orgunit", return_value=fk_org_orggunit
-    ) as get_user_mock:
-        await amqp_trigger_it_user(mock_context, uuid4(), None)
+    ) as get_org_unit_mock:
+        with patch("os2sync_export.main.is_relevant", return_value=is_relevant):
+            await amqp_trigger_it_user(mock_context, uuid4(), None)
+
     get_mock.assert_awaited_once()
-    get_user_mock.assert_called_once_with(orgunit_uuid, settings)
+
+    if is_relevant:
+        get_org_unit_mock.assert_called_once_with(orgunit_uuid, settings)
+        if overwritten_uuid:
+            os2sync_client.delete_orgunit.assert_called_once_with(orgunit_uuid)
+        else:
+            os2sync_client.delete_orgunit.assert_not_called()
+    else:
+        get_org_unit_mock.assert_not_called()
+        os2sync_client.delete_orgunit.assert_not_called()
     # Test that we call update_org_unit on events
     os2sync_client.update_org_unit(orgunit_uuid, fk_org_orggunit.json())
     # Ensure we won't delete the org_unit
-    os2sync_client.delete_orgunit.assert_not_called()
-
-
-@pytest.mark.asyncio
-@patch(
-    "os2sync_export.main.get_ituser_org_unit_and_employee_uuids",
-    return_value=(uuid4(), None),
-)
-@patch(
-    "os2sync_export.main.is_relevant",
-    return_value=True,
-)
-async def test_trigger_it_orgunit_delete_mo_uuid(
-    is_relevant_mock, get_mock, mock_context, mock_settings
-):
-    """Delete fk-org org_unit with MO uuid if a uuid is overwritten from an IT-account
-
-    Tests that if the found it-account(s) has different uuids to MOs
-    the account with MOs uuid is deleted
-    """
-    mock_context["user_context"]["settings"] = mock_settings
-    settings, _, os2sync_client = unpack_context(context=mock_context)
-    org_unit_uuid = get_mock.return_value[0]
-    with patch(
-        "os2sync_export.main.get_sts_orgunit", return_value=OrgUnit(Uuid=uuid4())
-    ) as get_user_mock:
-        await amqp_trigger_it_user(mock_context, uuid4(), None)
-    get_mock.assert_awaited_once()
-    # Test that we call update_org_unit on events
-    get_user_mock.assert_called_once_with(org_unit_uuid, settings)
-    # Ensure that we delete the fk-org org_unit with MO uuid as the unit has a different uuid in fk-org
-    os2sync_client.delete_orgunit.assert_called_with(org_unit_uuid)
